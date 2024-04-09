ce5985b17164d0a9443f2f2f5bb4af66c98a8556
Domroon <haeusser-dominik@web.de>
2024-04-09 11:20:48 +0200

    implement rotary encoder for changing frequency

--- main/main.ino
+++ main/main.ino
@@ -7,6 +7,12 @@
 #define DT              5
 int pos = 0;
 int pos_before = 0;
+int diff = 0;
+int period_dura_ms = 500;
+int frequency_hz = 1;
+int frequency_max_hz = 50;
+int frequency_min_hz = 4;
+int frequency_step = 1;
 
 // Clock LED
 bool clock = false;
@@ -42,7 +48,8 @@ class Timer{
     }
 };
 
-Timer ledTimer(100);
+Timer ledTimer(period_dura_ms);
+Timer serialTimer(1000);
 
 void setup() {
     Serial.begin(9600);
@@ -55,8 +62,8 @@ void setup() {
 void loop() {
     check_step_btn();
     set_led_state();
-    pos = convertGrayToBinary();
-    Serial.println(pos);
+    change_clk_frequency();
+    sendSerialInfo();
 }
 
 void check_step_btn(){
@@ -85,14 +92,63 @@ void set_led_state(){
 int convertGrayToBinary(){
   bool clk = digitalRead(CLK);
   bool dt = digitalRead(DT);
-  if(clk == 0 && dt == 0){
+  if(dt == 0 && clk == 0){
     return 0;
-  } else if(clk == 0 && dt == 1){
+  } else if(dt == 0 && clk == 1){
     return 1;
-  } else if(clk == 1 && dt == 1){
+  } else if(dt == 1 && clk == 1){
     return 2;
   } else {
     return 3;
+  }
+}
+
+float change_clk_frequency(){
+  pos = convertGrayToBinary();
+
+  // calculate the difference to previous position
+  diff = pos_before - pos;
+
+  // turned clockwise
+  if (((diff == -1) || (diff == 3))) {
+    pos_before = pos;
+    if (frequency_hz*1000 <= frequency_max_hz*1000) {
+      frequency_hz = frequency_hz + frequency_step; 
+      period_dura_ms = calc_period_duration_in_ms(frequency_hz);
+      ledTimer.changeDuration(period_dura_ms);
+    }
+            
+  // turned counter-clockwise
+  } else if (((diff == 1) || (diff == -3))) {
+      pos_before = pos;
+      if (frequency_hz*1000 > frequency_min_hz*1000){ 
+        frequency_hz = frequency_hz - frequency_step; 
+        period_dura_ms = calc_period_duration_in_ms(frequency_hz);
+        ledTimer.changeDuration(period_dura_ms);
+      }
+        
+  // in this case, an step has been missed
+  // (best practice: ignore it!)
+  } else if ((diff == 2) || (diff == -2)) {
+
+  }
+}
+
+float calc_period_duration_in_ms(int frequency){
+  float period_duration = (1/float(frequency)) * 1000;
+  return period_duration;
+}
+
+void sendSerialInfo(){
+  if(serialTimer.checkTimeOver() && clock){
+    Serial.print("Frequency: ");
+    Serial.print(frequency_hz);
+    Serial.println(" Hz");
+
+    Serial.print("Period-Duration: ");
+    Serial.print(period_dura_ms);
+    Serial.println(" ms");
+    serialTimer.restart();
   }
 }
 
